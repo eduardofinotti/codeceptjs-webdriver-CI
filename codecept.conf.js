09bd32716c886725d604547b01b6b5b776022fcf
Eduardo Finotti <eduardofinotti@Mac-do-Finotti.local>
2021-08-02 11:37:19 -0300

    first commit

---
+++ codecept.conf.js
@@ -0,0 +1,38 @@
+const { setHeadlessWhen } = require("@codeceptjs/configure");
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+exports.config = {
+  tests: "./*_test.js",
+  output: "./output",
+  helpers: {
+    WebDriver: {
+      url: "https://www.qazando.com.br",
+      browser: "chrome",
+    },
+  },
+  include: {
+    I: "./steps_file.js",
+  },
+  bootstrap: null,
+  mocha: {},
+  name: "curso-automacao-web-codeceptjs",
+  plugins: {
+    wdio: {
+      enabled: true,
+      services: ["selenium-standalone"],
+    },
+    pauseOnFail: {},
+    retryFailedStep: {
+      enabled: true,
+    },
+    tryTo: {
+      enabled: true,
+    },
+    screenshotOnFail: {
+      enabled: true,
+    },
+  },
+};
